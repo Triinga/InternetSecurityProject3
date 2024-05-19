a5d82f1632ed6fd1b83f33aef27fc5482594fecf
triinga <97124034+Triinga@users.noreply.github.com>
2024-05-19 20:39:18 +0200

    NSA-2: added functionality for selecting network interface

--- packet-analyzer.py
+++ packet-analyzer.py
@@ -1,3 +1,7 @@
+from scapy.all import IP, Ether
+import psutil
+from colorama import init, Fore, Style
+
 def analyze_packet(packet):
     global frame_number
     if IP in packet:
@@ -14,3 +18,14 @@ def analyze_packet(packet):
         print(f"{Fore.YELLOW}Protocol: {proto}   Size: {size} bytes")
         print(f"{Fore.GREEN}Ether Type: {ether_type}   Frame Number: {frame_number}   Frame Length: {frame_len} bytes")
         print("-" * 50)
+
+def select_network_interface():
+    interfaces = psutil.net_if_addrs()
+    print(f"{Fore.MAGENTA}Available network interfaces:")
+    for interface_name, _ in interfaces.items():
+        print(interface_name)
+    while True:
+        selected_interface = input(f"{Fore.YELLOW}Enter the name of the network interface you want to capture traffic on: ")
+        if selected_interface in interfaces:
+            return selected_interface
+        print(f"{Fore.RED}Invalid interface name. Please try again.\n")
